94b962d079a7d3574834d5a72e754982d93adeae
Gautam Billore <gautam billore>
2025-08-07 16:04:34 +0530

    corn updated

--- src/config/corn.js
+++ src/config/corn.js
@@ -1,18 +1,16 @@
-import { log } from 'console';
-import corn from 'cron';
-import https from 'http'
+import cron from 'cron';
+import https from 'https'; // or http if needed
 
+const job = new cron.CronJob("*/14 * * * *", function() {
+  https.get(process.env.API_URL, (res) => {
+    if (res.statusCode === 200) {
+      console.log("GET request sent successfully");
+    } else {
+      console.log("GET request failed", res.statusCode);
+    }
+  }).on("error", (e) => console.log("Error while sending request:", e));
+});
 
-const job = new corn.CronJob("*/14* * * *", function(){
-    https.get(process.env.API_URL,(res)=>{
-        if(res.statusCode===200)console.log("GET request sent succesfull");
-        else console.log("GET request failed",res.statusCode);
-        
-        
-    })
-    .on("error",(e)=> console.log("error while sending request ",e));
+job.start();
 
-
-})
-
-export default job; 
+export default job;
